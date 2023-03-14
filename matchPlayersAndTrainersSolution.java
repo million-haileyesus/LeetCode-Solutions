f06c1c0b4561600a9cfcb53664c358754c5c5cba
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-03-14 11:18:37 -0400

    Solutions

---
+++ matchPlayersAndTrainersSolution.java
@@ -0,0 +1,20 @@
+public class matchPlayersAndTrainersSolution {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int result = 0;
+        int i = 0; 
+        int j = 0;
+        for(; i < players.length && j < trainers.length; ) {
+            if(players[i] < trainers[j] + 1) {
+                result++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        return result;
+    }
+}
---
+++ mergeSortedArraySolution.java
@@ -0,0 +1,29 @@
+public class mergeSortedArraySolution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] result = new int[m+n];
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        for(; i < m && j < n; ) {
+        	
+            if(nums1[i] > nums2[j]) {
+                result[count++] = nums2[j++];
+            }
+            else {
+                result[count++] = nums1[i++];
+            }
+            
+        }
+        
+        while(i < m) {
+            result[count++] = nums1[i++];
+        }
+        
+        while(j < n) {
+            result[count++] = nums2[j++];
+        }
+       for(int k = 0; k < result.length; k++) {
+           nums1[k] = result[k];
+       }
+    }
+}
