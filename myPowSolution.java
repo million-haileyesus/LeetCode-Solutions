985caa3367cf8ac85a683fd3374be0a1529beee4
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-16 13:26:42 -0400

    Solution

--- myPowSolution.java
+++ myPowSolution.java
@@ -2,13 +2,13 @@ public class myPowSolution {
     public double myPow(double x, int n) {
         double result = 1.0;
         if(n > 0) {
-	        for(int i = 0; i < n; i++) {
+	        for(long i = 0; i < n; i++) {
 	            result *= x;
 	        }
         }
         else {
         	int size = -(n);
-        	for(int i = 0; i < size; i++) {
+        	for(long i = 0; i < size; i++) {
         		result *= 1/x;
         	}
         }
