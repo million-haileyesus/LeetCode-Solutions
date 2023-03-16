b6510729da671381002101f2b367313af550ae1f
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-15 21:24:34 -0400

    Solution

---
+++ runningSumOf1dArraySolution.java
@@ -0,0 +1,12 @@
+public class runningSumOf1dArraySolution {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        int temp = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i; j > -1; j--) {
+                result[i] += nums[j];
+            }
+        }
+        return result;
+    }
+}
