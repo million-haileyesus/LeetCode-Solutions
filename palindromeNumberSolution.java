9e345b29f1194998078cf7dae4286bbfbe95aad1
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-03-17 11:39:08 -0400

    Solution

---
+++ palindromeNumberSolution.java
@@ -0,0 +1,18 @@
+public class palindromeNumberSolution {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        boolean isPalindrome = true;
+        int size = s.length();
+        if(s.charAt(0) == '-') {
+            isPalindrome = false;
+        }
+        else {
+            for(int i = 0; i < size / 2; i++) {
+                if(!(s.charAt(i) == (s.charAt(size-i-1)))) {
+                    isPalindrome = false;
+                }
+            }
+        }
+        return isPalindrome;
+    }
+}
