ebaa1dbabd1ff334584efe13628f9705dec48bbe
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-03-17 12:40:30 -0400

    Solution

--- concatenationOfArraySolution.java
+++ concatenationOfArraySolution.java
@@ -1,4 +1,4 @@
-public class concatenationOfArraySolution {
+public class ConcatenationOfArraySolution {
     public int[] getConcatenation(int[] nums) {
         int[] result = new int[nums.length << 1];
         for(int i = 0; i < nums.length; i++) {
