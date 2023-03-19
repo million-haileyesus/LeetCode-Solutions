22f720340f320c0a85601013f9c876fcbcf75c02
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-19 17:46:17 -0400

    Solution

---
+++ PlusOneSolution.java
@@ -0,0 +1,22 @@
+class PlusOneSolution {
+    public int[] plusOne(int[] digits) {
+        if(digits[digits.length - 1] < 9) {
+            digits[digits.length - 1] = digits[digits.length - 1] + 1;
+        }
+        else {
+            int i = digits.length - 1;
+            while(i > 0 && digits[i] == 9) {
+                digits[i] = 0;
+                i--;
+            }
+            if(digits[i] == 9) {
+            	digits = new int[digits.length + 1];
+            	digits[1] = 0;
+            	i = 0;
+            }
+            digits[i] = digits[i] + 1;
+
+        }
+        return digits;
+    }
+}
