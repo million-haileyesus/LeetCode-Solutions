2da7602f4609c14a40f1eee9f42cd6802a19d975
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-23 14:28:11 -0400

    Solution

--- SearchInsertPositionSolution.java
+++ SearchInsertPositionSolution.java
@@ -1,10 +1,9 @@
 public class SearchInsertPositionSolution {
     public int searchInsert(int[] nums, int target) {
         int low = 0;
-        int result = 0;
         int high = nums.length - 1;
         while(low < high) {
-            int mid = (low + high) / 2;
+            int mid = (low + high) >> 1;
             if(target > nums[mid]) {
                 low = mid + 1;
             }
@@ -12,12 +11,11 @@ public class SearchInsertPositionSolution {
                 high = mid;
             }
         }
-        if(target == nums[high]) {
-	    result = high;
+        if(target > nums[high]) {
+            return high + 1;
 	}
 	else {
-	    result = (target < nums[high]) ? high : high + 1;
-	}
-        return result;
+            return high;
+        }
     }
 }
