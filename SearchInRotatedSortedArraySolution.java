0a30946051158f79f46fb3c02160d324c2776e40
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-03-26 22:05:10 -0400

    Solution

---
+++ SearchInRotatedSortedArraySolution.java
@@ -0,0 +1,12 @@
+public class earchInRotatedSortedArraySolution {
+    public int search(int[] nums, int target) {
+        int result = -1;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
