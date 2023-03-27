99edf620c4522b72a095cf9039c76cb4965ca12e
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-03-27 16:10:46 -0400

    Solution

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int findMin(int[] nums) {
+        int min = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] < nums[min]) {
+                min = i;
+            }
+        }
+        return nums[min];
+    }
+}
