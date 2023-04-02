eb379351f4c326feec35e95bd1c1aba8a36d8723
Million Seyoum Haileyesus <noillimsey@gmail.com>
2023-04-02 15:14:51 -0400

    Solution

---
+++ PowerOfThreeSolution.java
@@ -0,0 +1,10 @@
+public class PowerOfThreeSolution {
+    public boolean isPowerOfThree(int n) {
+        long i = 1;
+        while(i < n) {
+             i *= 3;
+        }
+        return i == n;
+    }
+}
+
