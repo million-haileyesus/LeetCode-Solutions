bf7d9a112318c7327878df0202bd0439426442e5
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-04-05 10:35:21 -0400

    Update SumOfDigitsOfStringAfterConvertSolution.java

--- SumOfDigitsOfStringAfterConvertSolution.java
+++ SumOfDigitsOfStringAfterConvertSolution.java
@@ -1,21 +1,21 @@
 public static int SumOfDigitsOfStringAfterConvertSolution(String s, int k) {
-		String res = "";
-		int result = 0;
-		int size = s.length();
-		for (int i = 0; i < size; i++) {
-			int rank = s.charAt(i) - (s.charAt(i) >= 'a' ? 'a' : 'A') + 1;
-			res += rank;
-		}
-		int i = 0;
-		while (i < k) {
-			int si = res.length();
-			result = 0;
-			for (int j = 0; j < si; j++) {
-				int rank = res.charAt(j) - '0';
-				result += rank;
-			}
-			res = Integer.toString(result);
-			i++;
-		}
-		return result;
+	String res = "";
+	int result = 0;
+	int size = s.length();
+	for (int i = 0; i < size; i++) {
+		int rank = s.charAt(i) - (s.charAt(i) >= 'a' ? 'a' : 'A') + 1;
+		res += rank;
 	}
+	int i = 0;
+	while (i < k) {
+		int si = res.length();
+		result = 0;
+		for (int j = 0; j < si; j++) {
+			int rank = res.charAt(j) - '0';
+			result += rank;
+		}
+		res = Integer.toString(result);
+		i++;
+	}
+	return result;
+}
