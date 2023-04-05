0c011f4e859ad9c63c5299db66e4daff93375592
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-04-04 21:45:20 -0400

    Solution

--- PowerOfFourSolution.java
+++ PowerOfFourSolution.java
@@ -2,7 +2,7 @@ public class PowerOfFourSolution {
     public boolean isPowerOfFour(int n) {
         long i = 1;
         while(i < n) {
-            i *= 4;
+            i <<= 2;
         }
         return i == n;
     }
