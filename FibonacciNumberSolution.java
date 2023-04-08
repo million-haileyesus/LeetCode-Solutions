a85920fccba648babda1131efdfd05b486a1f20c
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-04-07 20:07:27 -0400

    Solution

---
+++ FibonacciNumberSolution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int result = n;
+        int f1 = 0;
+        int f2 = 1;
+        for(int i = 2; i < n + 1; i++) {
+            result = f1 + f2;
+            f1 = f2;
+            f2 = result;
+        }
+        return result;
+    }
+}
