8035cc3f165e5670213b2be683db0d7746d3692a
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-04-07 20:18:07 -0400

    Solution

---
+++ SquaresOfASortedArray.java
@@ -0,0 +1,9 @@
+public class SquaresOfASortedArraySolution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
