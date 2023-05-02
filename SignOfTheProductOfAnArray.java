ee646e798a57a4779087af9ed49b6dfb9545db1e
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-05-01 21:24:50 -0400

    Solution

---
+++ SignOfTheProductOfAnArray.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int arraySign(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                return 0;
+            }
+            else if(nums[i] < 0) {
+                result++;
+            }
+        }
+        return (result % 2 == 0) ? 1 : -1; 
+    }
+}
