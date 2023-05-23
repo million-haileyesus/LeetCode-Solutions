e896dca39290f6a66e119492d2f50463efea6a6f
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-05-23 12:02:08 -0400

    Solution

---
+++ DistributeCandiesSolution.java
@@ -0,0 +1,14 @@
+class DistributeCandiesSolution {
+    public int distributeCandies(int[] candyType) {
+        Arrays.sort(candyType);
+        int result = 0;
+        int count = 1;
+        for(int i = 0; i < candyType.length - 1; i++) {
+            if(candyType[i] != candyType[i + 1]) {
+                count++;
+            }
+        }
+        result = count > candyType.length >> 1 ? candyType.length >> 1 : count;
+        return result;
+    }
+}
