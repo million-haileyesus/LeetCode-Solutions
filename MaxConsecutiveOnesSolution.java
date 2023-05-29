714d7d2eb0af6e83c6f4b45ece0ab8a88f14391e
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-05-29 10:47:21 -0400

    Solution

---
+++ MaxConsecutiveOnesSolution.java
@@ -0,0 +1,18 @@
+public class MaxConsecutiveOnesSolution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
+                count++;
+            }
+            else {
+                count = 0;
+            }
+            if(count > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
