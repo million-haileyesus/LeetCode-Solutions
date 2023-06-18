adf56affd04295e0da5be322c0ec70796a83e968
Million Haileyesus <107280315+million-haileyesus@users.noreply.github.com>
2023-06-18 14:11:17 -0400

    Update AddDigitsSolution.java

--- AddDigitsSolution.java
+++ AddDigitsSolution.java
@@ -1,5 +1,5 @@
 public class AddDigitsSolution {
     public int addDigits(int num) {
-        return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
+        return num = 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
     }
 }
